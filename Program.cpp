9ba8f9c10eadfbf52f131dfdef2f89fa9af58ed2
Любимова Алиса <lubimovaalisa57@gmail.com>
2025-11-27 11:22:30 +0300

    Создала program.cpp

---
+++ Program.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int size;
+    cout << "Сколько чисел в списке? ";
+    cin >> size;
+    
+    int numbers[size];
+    
+    cout << "Введите " << size << " чисел: ";
+    for (int i = 0; i < size; i++) {
+        cin >> numbers[i];
+    }
+    
+    int max = numbers[0];
+    for (int i = 1; i < size; i++) {
+        if (numbers[i] > max) {
+            max = numbers[i];
+        }
+    }
+    
+    cout << "Максимум: " << max << endl;
+    
+    return 0;
+}
